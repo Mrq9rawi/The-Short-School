0a90b90c7aaf48bf44d5dbc15f9efbe72dd73e1f
CShingiro <cshingiro7202@conestogac.on.ca>
2021-12-04 19:31:28 -0500

    PHP Commit from Christian

---
+++ teacher.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+	    <link rel="stylesheet" href="master.css">
+	    <link rel="preconnect" href="https://fonts.googleapis.com">
+	    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+	    <link href="https://fonts.googleapis.com/css2?family=STIX+Two+Text:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet">
+	    <title>Teacher Results</title>
+    </head>
+    <body>
+        <?php
+            
+
+        echo "<details>
+                <summary>$teacher_name Performance</summary>
+            </details>
+            <details>
+                <summary>Cumulative Teacher average</summary>
+                <table>
+                    <thead>$teacher_table</thead>
+                    <tbody></tbody>
+                </table>
+            </details>"
+        ?>
+    </body>
+</html>
