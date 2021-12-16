984f0ef91920fd54452d9c8e480a986426e8ea70
Mrq9rawi <moh.abdullah02@gmail.com>
2021-12-15 23:28:28 -0500

    table styled

--- student.php
+++ student.php
@@ -12,6 +12,11 @@
 	    <link href="https://fonts.googleapis.com/css2?family=STIX+Two+Text:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" type="text/css">
 	    <title>Student Results</title>
     </head>
+		<style>
+			body {
+				padding: 20px;
+			}
+		</style>
     <body>
         <?php
             require "database.php";
